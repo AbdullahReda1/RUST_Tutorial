7a3db80b0d0dd0e8abc532a13419333834e9afee
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-11-02 23:42:26 +0200

    3.1

--- CommonProgrammingConcepts/Variables_and_Mutability/src/main.rs
+++ CommonProgrammingConcepts/Variables_and_Mutability/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
+
+/*
+If (let x = 5;):
+----------------
+    error[E0384]: cannot assign twice to immutable variable `x`
+    --> src/main.rs:4:5
+    |
+    2 |     let x = 5;
+    |         - first assignment to `x`
+    3 |     println!("The value of x is: {x}");
+    4 |     x = 6;
+    |     ^^^^^ cannot assign twice to immutable variable
+    |
+    help: consider making this binding mutable
+    |
+    2 |     let mut x = 5;
+    |         +++
+*/
